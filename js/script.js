44aef62bb95793b65a6e6442f63eb38733af895c
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2021-12-16 17:38:10 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -23,3 +23,56 @@ if (parola === parolaReverseUnita) {
 }
 
 // -----------------------------------------------------------------------
+
+// chiedo all'utente di sciegliere pari o discpari
+const user = prompt('Sciegli pari o dispari?');
+console.log ('Ho scelto ' + user);
+
+// chiedo all'utente un numero da 1 a 5
+const userNumber = parseInt(prompt('un numero da 1 a 5?'));
+console.log ('il numero scelto da me è '+ userNumber)
+
+//  genero un numero random da 1 a 5 per la cpu
+function randomNumber(minNum, maxNum) {
+    const result = Math.floor(Math.random() * ((maxNum + 1) - minNum)) + minNum;
+    return result;
+}
+const cpuNumber = randomNumber(1, 5);
+console.log('il numero generato per la cpu è ' + cpuNumber);
+
+//  if controllo pari o dispari
+function pariDispari (value) {
+    if (value %2 == 0){
+		return true;
+    } else {
+		return false;
+}}
+
+// somma dei due elementi
+const somma = userNumber + cpuNumber;
+let calc = pariDispari(somma);
+
+// creo la variabile esito che avrà come valore (2 = pari) o (1 = dispari)
+let esito;
+
+// creo un if per controllare se vince pari o dispari 
+if (calc) {
+    console.log('Ha vinto il numero pari');
+    esito = 2;
+} else {
+    console.log('Ha vinto il numero dispari');
+    esito = 1;
+}
+
+// creo un if per controllare chi ha vinto 
+if (user === 'pari' && esito === 2) {
+    console.log('L\'utente vince')
+} else if (user === 'dispari' && esito === 1) {
+    console.log('L\'utente vince')
+} else {
+     console.log('La cpu vince')
+
+}
+
+
+

47adf467d014683b65da9ed593a9cf4cfd0fb420
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2021-12-16 16:33:30 +0100

    prima parte

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
+//  definisco la variabile output per html
 const output = document.getElementById('output');
-const parola = prompt('Inserisci una parola palindroma!');
+
+// chiedo la parola
+const parola = prompt('dimmi una parola e ti dirò se è palindroma!');
+
+// divido la parola in caratteri con .split
 const parolaDivisa = parola.split('');
+
+// cambio l'ordiene dei carattere con .reverse
 const parolaReverse = parolaDivisa.reverse();
+
+// unisco i caratteri rovesciati con .join
 const parolaReverseUnita = parolaReverse.join('')
 
+// con un if capisco se la parola al contrario è uguale a quella inserita
 if (parola === parolaReverseUnita) {
-     output.innerHTML = ('La tua parola è palindroma')
+    // stampo esito
+     output.innerHTML = ('La tua parola è palindroma!')
 } else {
-     output.innerHTML = ('La tua parola non è palindroma')
+    // stampo esito
+     output.innerHTML = ('La tua parola non è palindroma :(')
 }
+
+// -----------------------------------------------------------------------
